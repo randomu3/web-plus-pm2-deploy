c261e6a4bb4fa00e84fea2d614d1d01b54f25d57
randomu3 <demiz99@mail.ru>
2023-07-15 17:49:52 +0000

    fixed paths of app.ts

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-	  apps : [{
-		      name: "backend",
-		      script: "./src/app.ts", // Или укажите другой путь к вашему главному файлу приложения
-		      instances: "max",
-		      env: {
-			            NODE_ENV: "development",
-			          },
-		      env_production: {
-			            NODE_ENV: "production",
-			          }
-		    }],
-	  deploy : {
-		      production : {
-			            user : "demiz", // Ваш пользователь на сервере
-			            host : "api.grottie.nomoredomains.work", // Ваш хост
-			            ref  : "origin/master",
-			            repo : "git@github.com:randomu3/web-plus-pm2-deploy.git", // Замените на свой репозиторий
-			            path : "/home/demiz/web-plus-pm2-deploy/backend", // Путь до папки с проектом
-			      	"post-deploy" : "npm install && pm2 reload /home/demiz/web-plus-pm2-deploy/backend/ecosystem.config.js --env production"
-			          }
-		    }
+	          apps : [{
+			                        name: "backend",
+			                        script: "./src/app.ts", // Или укажите другой путь к вашему главному файлу приложения
+			                        instances: "max",
+			                        env: {
+							                                    NODE_ENV: "development",
+							                                  },
+			                        env_production: {
+							                                    NODE_ENV: "production",
+							                                  }
+			                      }],
+	          deploy : {
+			                        production : {
+							                                    user : "demiz", // Ваш пользователь на сервере
+							                                    host : "api.grottie.nomoredomains.work", // Ваш хост
+							                                    ref  : "origin/master",
+							                                    repo : "git@github.com:randomu3/web-plus-pm2-deploy.git", // Замените на свой репозиторий
+							                                    path : "/home/demiz/web-plus-pm2-deploy/backend", // Путь до папки с проектом
+							                                    "post-deploy" : "npm install && pm2 reload /home/demiz/web-plus-pm2-deploy/backend/ecosystem.config.js --env production"
+							                                  }
+			                      }
 }
 
