b53f0bdf93b075dc8c5982d46e9a30f48a607ef2
randomu3 <demiz99@mail.ru>
2023-07-16 16:44:31 +0000

    fixed frontend cfg

--- ecosystem-frontend.config.js
+++ ecosystem-frontend.config.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 
+const {
+	DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
 module.exports = {
 	  apps: [
 		      {
 			            name: "frontend-service",
 			            script: "serve",
 			            env: {
-					            PM2_SERVE_PATH: '/home/demiz/web-plus-pm2-deploy/frontend/build',
+					            PM2_SERVE_PATH: `${DEPLOY_PATH}/frontend/build`,
 					            PM2_SERVE_PORT: 8000,
 					          },
 			          },
@@ -14,13 +18,13 @@ module.exports = {
 
 	  deploy: {
 		      production: {
-			            user: 'demiz',
-			            host: '84.201.162.24',
-			            ref: 'origin/master',
+			            user: DEPLOY_USER,
+			            host: DEPLOY_HOST,
+			            ref: DEPLOY_REF,
 			            repo: 'https://github.com/randomu3/web-plus-pm2-deploy.git',
-			            path: '/home/demiz/web-plus-pm2-deploy/frontend',
-			            "post-deploy":
-			              "npm install && npm run build && pm2 startOrRestart ecosystem-frontend.config.js",
+			            path: `${DEPLOY_PATH}/frontend`,
+					'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+			            'post-deploy': 'npm i && npm run build',
 			          },
 		    },
 };
