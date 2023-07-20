9ccf2d25ac84358d552b3cf88757b1984d3ba5f5
randomu3 <demiz99@mail.ru>
2023-07-20 16:05:16 +0500

    fixed link for backend api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,7 +139,7 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.grottie.nomoredomains.work/');
+  const api = new Api('https://api.grottie.nomoredomains.work');
   
   export default api;
   
