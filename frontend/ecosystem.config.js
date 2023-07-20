1d92e1e2152c68a488acd9250d9f4a8748becffc
randomu3 <demiz99@mail.ru>
2023-07-20 15:42:09 +0500

    del pre-deploy

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: "./.env.deploy" });
+
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && pwd && npm i && npm run build',
+    },
+  },
+}
